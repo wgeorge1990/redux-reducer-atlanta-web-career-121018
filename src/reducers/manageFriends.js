730054222a1eefdd1eb23aa50730105877f503df
wgeorge1990 <williameverettgeorge@icloud.com>
2019-03-10 16:07:41 -0400

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,11 @@
-export function manageFriends(state, action){
+export function manageFriends(prevstate = {friends: []}, action){
+    switch (action.type) {
+        case 'ADD_FRIEND':
+            return ({...prevstate, friends: [...prevstate.friends, action.friend]})
+        case 'REMOVE_FRIEND':
+            let filteredFriends = prevstate.friends.filter(friend => friend.id !== action.id) 
+            return ({...prevstate, friends:  filteredFriends})
+        default:
+            return prevstate
+    }
 }
--- src/reducers/managePresents.js
+++ src/reducers/managePresents.js
@@ -1,2 +1,9 @@
-export function managePresents(state, action){
+export function managePresents(prevstate, action){
+    switch (action.type) {
+        case 'INCREASE':
+            return {numberOfPresents: prevstate.numberOfPresents + 1}
+        default:
+            return prevstate
+    }
 }
+
